c303c3ee3a8cb3f1d36e47d3c55aba16db8e8f05
Vince <quocbaohoang90@gmail.com>
2017-08-17 21:40:05 +0800

    Update setup for security configuration

--- src/main/java/com/video/upload/MyVideoUpload/config/SecurityConfiguration.java
+++ src/main/java/com/video/upload/MyVideoUpload/config/SecurityConfiguration.java
@@ -1,4 +1,17 @@
 package com.video.upload.MyVideoUpload.config;
 
-public class SecurityConfiguration {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider();
+    }
 }
--- src/main/java/com/video/upload/MyVideoUpload/controller/LoginController.java
+++ src/main/java/com/video/upload/MyVideoUpload/controller/LoginController.java
@@ -1,4 +1,16 @@
 package com.video.upload.MyVideoUpload.controller;
 
+import com.video.upload.MyVideoUpload.service.LoginService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+    private LoginService loginService;
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String login(){
+        return "hello";
+    }
 }
