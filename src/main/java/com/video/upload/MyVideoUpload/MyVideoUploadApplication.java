b8a243cf30ae5c513fb7938cbbc16798d824fbc7
Vince Hoang <quocbaohoang90@gmail.com>
2017-08-17 16:37:04 +0800

    Initial commit, including configs for Gradle

---
+++ src/main/java/com/video/upload/MyVideoUpload/MyVideoUploadApplication.java
@@ -0,0 +1,12 @@
+package com.video.upload.MyVideoUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVideoUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVideoUploadApplication.class, args);
+	}
+}
