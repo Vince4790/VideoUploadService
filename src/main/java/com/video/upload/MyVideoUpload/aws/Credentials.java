75139a72287e7059b8e41864d9dd64a6eacb1889
Vince4790 <quocbaohoang90@gmail.com>
2017-08-22 02:48:20 +0800

    Delete Credentials.java
    temporary remove credentials file

--- src/main/java/com/video/upload/MyVideoUpload/aws/Credentials.java
+++
@@ -1,7 +0,0 @@
-package com.video.upload.MyVideoUpload.aws;
-
-public class Credentials {
-    public static final String access_key_id= "AKIAIZAMIJ55Z7OEN7IA";
-
-    public static final String secret_access_key ="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";
-}
